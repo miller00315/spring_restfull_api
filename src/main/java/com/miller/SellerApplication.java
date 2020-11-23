2327e5e84ef4f5b4c7cc210aeee87e96db44b96e
Miller Cesar de Oliveira <miller00315@gmail.com>
2020-11-23 20:40:41 -0300

    Removendo importações desnecessárias

--- src/main/java/com/miller/SellerApplication.java
+++ src/main/java/com/miller/SellerApplication.java
@@ -1,12 +1,7 @@
 package com.miller;
 
-import com.miller.domain.entity.Client;
-import com.miller.domain.repository.Clients;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SellerApplication {
