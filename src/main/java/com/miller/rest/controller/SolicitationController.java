bdafbd29da9ba09af8bab3673cc075d1442e912c
Miller Cesar de Oliveira <miller00315@gmail.com>
2020-11-23 21:39:29 -0300

    Ajustando Nome de classe

--- src/main/java/com/miller/rest/controller/SolicitationController.java
+++ src/main/java/com/miller/rest/controller/SolicitationController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/solicitations")
-public class SolicitationConntroller {
+public class SolicitationController {
 
     private final Solicitations repository;
 
-    public SolicitationConntroller(Solicitations repository) {
+    public SolicitationController(Solicitations repository) {
         this.repository = repository;
     }
 }
